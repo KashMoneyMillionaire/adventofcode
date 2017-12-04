b1ea9cdf116b906f46223d65d2c43c93728cb29b
Kash Cummings <kashleec@gmail.com>
2017-12-04 12:53:50 -0600

    added comments for day 4

--- day4/code.exs
+++ day4/code.exs
@@ -1,7 +1,7 @@
 "input.txt"
-    |> File.read!
-    |> String.split("\n")
-    |> Enum.map(fn(r) -> String.split(r) end)
-    |> Enum.filter(fn(r) -> (Enum.uniq(r) |> length) == length(r) end)
-    |> Enum.count
-    |> IO.inspect
+    |> File.read! # read in contents of file. Output: string -> string
+    |> String.split("\n") # split all text by new line. Output: string[]
+    |> Enum.map(fn(row) -> String.split(row) end) # split each row by whitespace. Output: string[][]
+    |> Enum.filter(fn(row) -> (Enum.uniq(row) |> length) == (row |> length) end) # keep items where the count of unique items is the same as the total number of items
+    |> Enum.count # count number of rows left
+    |> IO.inspect # output
--- day4/code2.exs
+++ day4/code2.exs
@@ -1,7 +1,15 @@
 "input.txt"
-    |> File.read!
-    |> String.split("\n")
-    |> Enum.map(fn(r) -> String.split(r) |> Enum.map(fn(i) -> i |> String.codepoints |> Enum.sort |> to_string end) end)
-    |> Enum.filter(fn(r) -> (Enum.uniq(r) |> length) == length(r) end)
-    |> Enum.count
-    |> IO.inspect
+    |> File.read! # read in contents of file. Output: string -> string
+    |> String.split("\n") # split all text by new line into rows. Output: string -> string[]
+    |> Enum.map(fn(row) -> 
+                    String.split(row) # split each row by whitespace. Output: string -> string[]
+                        |> Enum.map(fn(i) -> 
+                                        i 
+                                            |> String.codepoints # turn string into char array. Output: string -> char[] (kinda)
+                                            |> Enum.sort # sort the char array alphabetically. Output char[] -> char[]
+                                            |> to_string # turn back into string. Output char[] -> string
+                                    end) # sort each string in the row by its characters alphabetically
+                end) 
+    |> Enum.filter(fn(row) -> (Enum.uniq(row) |> length) == length(row) end) # keep items where the count of unique items is the same as the total number of items
+    |> Enum.count # count number of rows left
+    |> IO.inspect # output
