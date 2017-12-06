f6a7df215658892befed56302047d6a37997697d
Kash Cummings <kashleec@gmail.com>
2017-12-06 12:13:16 -0600

    fixes for 5

--- day5/code.exs
+++ day5/code.exs
@@ -1,71 +1,93 @@
 defmodule Day5 do
 
-    def jump([h | t]) do
-        jump([], h, t, h, 0)
-    end
+    # def jump([h | t]) do
+    #     jump([], h, t, h, 0)
+    # end
 
-    def jump(front, currentItem, back, jumpSize, iterationCount) do
+    # def jump(front, currentItem, back, jumpSize, iterationCount) do
 
-        IO.inspect front
-        IO.inspect currentItem
-        IO.inspect back
-        IO.inspect jumpSize
-        IO.inspect iterationCount
-        IO.puts "--\n"
+    #     IO.inspect front
+    #     IO.inspect currentItem
+    #     IO.inspect back
+    #     IO.inspect jumpSize
+    #     IO.inspect iterationCount
+    #     IO.puts "--\n"
 
-        if length(back) < jumpSize do
-            iterationCount
+    #     if length(back) < jumpSize do
+    #         iterationCount
+    #     else
+
+    #         # (0) 3 0 1 -3      0
+    #         # (1) 3 0 1 -3      1
+    #         # 2 (3) 0 1 -3      2
+    #         # 1 4 0 1 (-3)      3
+    #         # 1 (4) 0 1 -2      4
+    #         # exit              5
+
+    #         cond do
+    #             jumpSize == 0 ->
+    #                 jump(front, currentItem + 1, back, currentItem, iterationCount + 1)
+    #             jumpSize < 0 -> 
+    #                 {f, m, b} = three_parts(front, jumpSize)
+    #                 jump(f, m, b ++ back, 0, iterationCount)
+    #             jumpSize > 0 ->
+    #                 {f, m, b} = three_parts(back, jumpSize)
+    #                 jump(front ++ f, m, b, 0, iterationCount)
+    #         end
+    #     end
+    # end
+
+    # defp three_parts([h | t], 0) do
+    #     IO.puts "three_parts/2 0"
+    #     { [], h, t }
+    # end
+
+    # defp three_parts([h | t], n) do
+    #     IO.puts "three_parts/2 n"
+    #     three_parts([], h, t, n)
+    # end
+
+    # defp three_parts(front, middle, [bh | bt], 1) do
+    #     IO.puts "three_parts/4 1"
+    #     IO.inspect front, label: "front"
+    #     IO.inspect middle, label: "middle"
+    #     IO.inspect bh, label: "bh"
+    #     IO.inspect bt, label: "bt"
+    #     IO.puts "--\n"
+    #     { front ++ [middle], bh, bt }
+    # end
+
+    # defp three_parts(front, middle, [bh | bt], n) do
+    #     IO.puts "\nthree_parts/4"
+    #     IO.inspect front, label: "front"
+    #     IO.inspect middle, label: "middle"
+    #     IO.inspect bh, label: "bh"
+    #     IO.inspect bt, label: "bt"
+    #     IO.inspect n, label: "n"
+    #     IO.puts "--\n"
+    #     three_parts(front ++ [middle], bh, bt, n-1 )
+    # end
+
+    def jump(list, offset, inc) do
+
+        if offset >= length(list) do
+            inc
         else
+            jump = list |> Enum.at(offset)
+            nextlist = list 
+                |> Enum.with_index 
+                # |> IO.inspect
+                |> Enum.map(fn({v, k}) -> if k == offset do v + 1 else v end end)
 
-            # (0) 3 0 1 -3      0
-            # (1) 3 0 1 -3      1
-            # 2 (3) 0 1 -3      2
-            # 1 4 0 1 (-3)      3
-            # 1 (4) 0 1 -2      4
-            # exit              5
+            # IO.inspect list
+            # # IO.inspect nextlist
+            # IO.puts offset
+            # IO.puts jump
+            # IO.puts inc
+            # IO.puts "\n"
 
-            cond do
-                jumpSize == 0 ->
-                    jump(front, currentItem + 1, back, currentItem, iterationCount + 1)
-                jumpSize < 0 -> 
-                    {f, m, b} = three_parts(front, jumpSize)
-                    jump(f, m, b ++ back, 0, iterationCount)
-                jumpSize > 0 ->
-                    {f, m, b} = three_parts(back, jumpSize)
-                    jump(front ++ f, m, b, 0, iterationCount)
-            end
+            jump(nextlist, offset + jump, inc + 1)
         end
-    end
-
-    defp three_parts([h | t], 0) do
-        IO.puts "three_parts/2 0"
-        { [], h, t }
-    end
-
-    defp three_parts([h | t], n) do
-        IO.puts "three_parts/2 n"
-        three_parts([], h, t, n)
-    end
-
-    defp three_parts(front, middle, [bh | bt], 1) do
-        IO.puts "three_parts/4 1"
-        IO.inspect front, label: "front"
-        IO.inspect middle, label: "middle"
-        IO.inspect bh, label: "bh"
-        IO.inspect bt, label: "bt"
-        IO.puts "--\n"
-        { front ++ [middle], bh, bt }
-    end
-
-    defp three_parts(front, middle, [bh | bt], n) do
-        IO.puts "\nthree_parts/4"
-        IO.inspect front, label: "front"
-        IO.inspect middle, label: "middle"
-        IO.inspect bh, label: "bh"
-        IO.inspect bt, label: "bt"
-        IO.inspect n, label: "n"
-        IO.puts "--\n"
-        three_parts(front ++ [middle], bh, bt, n-1 )
     end
 
 end
@@ -77,5 +99,12 @@ directions = "input.txt"
 
 # IO.inspect directions
 
-Day5.jump(directions)
+Day5.jump(directions, directions |> Enum.at(0), 0)
     |> IO.inspect
+
+    #         # (0) 3 0 1 -3      0
+    #         # (1) 3 0 1 -3      1
+    #         # 2 (3) 0 1 -3      2
+    #         # 2 4 0 1 (-3)      3
+    #         # 2 (4) 0 1 -2      4
+    #         # exit              5
--- day5/code2.exs
+++ day5/code2.exs
@@ -1,4 +1,48 @@
-"input.txt"
+defmodule Maze do
+  def stream(jumps, offset) do
+    Stream.unfold({0, build_map(jumps), offset}, &next/1)
+  end
+
+  defp build_map(jumps) do
+    jumps
+    |> Enum.with_index
+    |> Enum.map(fn {v, i} -> {i, v} end)
+    |> Enum.into(%{})
+  end
+
+  def next({index, jumps, offset}) do
+    case jumps[index] do
+      nil ->
+        nil
+      jump ->
+        next = Map.put(jumps, index, jump + offset.(jump))
+        {jump, {index + jump, next, offset}}
+    end
+  end
+
+end
+
+# input = System.argv
+# |> List.first
+# |> String.split("\n", trim: true)
+# |> Enum.map(&String.to_integer/1)
+
+input = "input.txt"
     |> File.read! # read in contents of file. Output: string -> string
-    |> String.split("\n") # split all text by new line. Output: string[]
-    
+    |> String.split("\r\n") # split all text by new line. Output: string[]
+    |> Enum.map(&String.to_integer/1)
+
+
+# Part one
+input
+|> Maze.stream(fn _ -> 1 end)
+|> Enum.to_list
+|> length
+|> IO.inspect
+
+# Part two
+input
+|> Maze.stream(fn n -> if n >= 3, do: -1, else: 1 end)
+|> Enum.to_list
+|> length
+|> IO.inspect
