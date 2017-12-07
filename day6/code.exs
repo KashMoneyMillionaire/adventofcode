8192c152d3d42d8638281b5d6317284241b22c5e
Kash Cummings <kashleec@gmail.com>
2017-12-06 22:45:07 -0600

    day 6 complete

---
+++ day6/code.exs
@@ -0,0 +1,49 @@
+defmodule MemoryBanks do
+    def count_cycles(blocks) do
+        blocks
+            |> redistributions
+            |> Enum.reduce_while(%{}, fn
+                (redistribution, map) ->
+                    case map[redistribution] do
+                        nil ->
+                            {:cont, Map.put_new(map, redistribution, :ok)}
+                        _ ->
+                            {:halt, { Map.keys(map) |> length, redistribution } }
+                    end
+            end)
+    end
+
+    def redistributions(blocks) do
+        Stream.iterate(blocks, &redistribute/1)
+    end
+
+    defp redistribute(blocks) do
+        {max, max_index} = blocks
+            |> Enum.with_index
+            |> Enum.max_by(&elem(&1, 0))
+        Stream.cycle([1])
+            |> Enum.take(max)
+            |> Enum.with_index
+            |> Enum.reduce(blocks, fn
+                ({n, index}, blocks) ->
+                    index = rem(index + max_index + 1, length(blocks))
+                    List.update_at(blocks, index, &(&1 + n))
+            end)
+            |> List.update_at(max_index, &(&1 - max))
+    end
+end
+
+input = "input.txt"
+    |> File.read!
+    |> String.split
+    |> Enum.map(&String.to_integer/1)
+
+# Part one
+{length, cycleNotice} = input
+    |> MemoryBanks.count_cycles
+    |> IO.inspect
+
+# Part two
+cycleNotice
+    |> MemoryBanks.count_cycles
+    |> IO.inspect
