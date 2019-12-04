9bf2072df52b17067b77e733971375d0cca92fa8
kashmoneymillionaire <kashleec@gmail.com>
2019-12-04 11:49:00 -0600

    day 3 finished!

--- 2019/Utilities/Library.fs
+++ 2019/Utilities/Library.fs
@@ -2,4 +2,11 @@
 
 module Ut = 
     let SplitLinesSplitOn (splitBy: char) = 
-        System.IO.File.ReadLines("input.txt") |> Seq.map (fun x -> x.Split(splitBy));
+        System.IO.File.ReadLines("input.txt") |> Seq.map (fun x -> x.Split(splitBy)) |> Seq.toList;
+
+    
+    let stringAsInt : string -> int = int
+
+    let charsAsInt : char list -> int  = fun (chars: char list) -> 
+        let str = new System.String(List.toArray(chars)) 
+        stringAsInt(str)
--- 2019/day-03/Day03/Program.fs
+++ 2019/day-03/Day03/Program.fs
@@ -1,14 +1,87 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open Utilities
+open Utilities.Ut
 open System
+
+type Coordinate = { X: int; Y: int; } 
+
+type Movement = { Direction: char; Distance: int;}
+
+let origin = {X = 0; Y = 0}
+
+let asMovement (dir::dist) = { Direction = dir; Distance = charsAsInt(dist)}
+
+//let mapAsMovement: seq<string> -> seq<Movement> = Seq.map(asMovement)
+let mapAsMovement = Seq.map(Seq.toList >> asMovement)
+
+let getNewCoordinate (movement: Movement, co: Coordinate) =
+    match movement.Direction with 
+    | 'R' -> { co with X = co.X + movement.Distance } 
+    | 'L' -> { co with X = co.X - movement.Distance } 
+    | 'U' -> { co with Y = co.Y + movement.Distance } 
+    | 'D' -> { co with Y = co.Y - movement.Distance } 
+    | _ -> failwith "Bad direction"
+
+
+let coordinatesFromMove (movement: Movement, co: Coordinate) =
+    match movement.Direction with 
+    | 'R' -> seq { for i in co.X .. co.X + movement.Distance -> {co with X = i}}
+    | 'L' -> seq { for i in co.X - movement.Distance .. co.X -> {co with X = i}}
+    | 'U' -> seq { for i in co.Y .. co.Y + movement.Distance -> {co with Y = i}}
+    | 'D' -> seq { for i in co.Y - movement.Distance .. co.Y -> {co with Y = i}} 
+    | _ -> failwith "Bad direction"
+
+let manhattanDistance (co1: Coordinate) (co2: Coordinate) =
+    Math.Abs(co1.X - co2.X) + Math.Abs(co1.Y - co2.Y)
+
+let mapToManhattanDistanceFromOrigin = origin |> (Seq.map << manhattanDistance)
+
+let shortestManhattanDistanceFromOrigin coordinates = coordinates |> mapToManhattanDistanceFromOrigin |> Seq.min
+
+let findsEnd (movement, start, finish) = 
+    let path = coordinatesFromMove(movement, start)
+    match path |> Seq.contains(finish) with
+    | true  -> (true, manhattanDistance start finish)
+    | false -> (false, movement.Distance)
+
+let rec walkingDistance (start: Coordinate, finish: Coordinate) (directions: Movement list) =
+    match directions with
+    | [] -> failwith "Couldn't find finish"
+    | hd::tail -> 
+        let r = findsEnd(hd, start, finish)
+
+        match (r, tail) with
+        | ((true, distance), _) -> distance
+        | ((false, _), []) -> failwith "No more directions"
+        | ((false, distance), _) -> distance + (tail |> walkingDistance(getNewCoordinate(hd, start), finish))
+
+let walkingDistanceFromOrigin finish movements = movements |> walkingDistance(origin, finish)
+
+
+let rec iterate (current: Coordinate) (hd::tail: Movement list) =
+    coordinatesFromMove(hd, current) 
+    |> Seq.append <|
+        match tail.Length with
+        | 0 -> Seq.empty<Coordinate>
+        | _ -> tail |> iterate (getNewCoordinate(hd, current))
 
 [<EntryPoint>]
 let main argv =
-    let input = Ut.SplitLinesSplitOn (',')
-    wire1 = input
-    printfn "%A" input
-    0 // return an integer exit code
+    let input = SplitLinesSplitOn (',')
+    let dir1 = input |> Seq.item(0) |> mapAsMovement |> Seq.toList
+    let dir2 = input |> Seq.item(1) |> mapAsMovement |> Seq.toList
+    let wire1 = dir1 |> iterate(origin) |> set
+    let wire2 = dir2 |> iterate(origin) |> set
     
-
-
+    // Part 1
+    let intersections = wire1 |> Set.intersect(wire2) |> Set.remove(origin)
+    printfn "Part 1: %A" (intersections |> shortestManhattanDistanceFromOrigin)    
+    
+    // Part 2
+    let z = intersections |> Seq.map(fun x -> walkingDistanceFromOrigin(x)) 
+    let a = z |> Seq.map(fun x -> x(dir1))
+    let b = z |> Seq.map(fun x -> x(dir2))
+    let lengths = Seq.zip a b
+    printfn "Part 1: %A" (lengths |> Seq.map(fun (l, r) -> l + r) |> Seq.min)    
+    
+    0
