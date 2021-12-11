eeac17ca4d3d77047eebfa48e5410a69e1ec121d
Kash Cummings <kashleec@gmail.com>
2021-12-11 00:02:46 -0500

    update program to auto run

--- 2021/Program.fs
+++ 2021/Program.fs
@@ -1,24 +1,29 @@
 ﻿module AdventOfCode._2021
 
 open System
+open System.Reflection
 
 let rec prompt forever =
     printfn "Which day would you like to run?"
-    
-    match Console.ReadLine() with
-                | "1" -> Day01.solve()
-                | "2" -> Day02.solve()
-                | "3" -> Day03.solve()
-                | "4" -> Day04.solve()
-                | "5" -> Day05.solve()
-                | "6" -> Day06.solve()
-                | "7" -> Day07.solve()
-                | "8" -> Day08.solve()
-                | "9" -> Day09.solve()
-                | "10" -> Day10.solve()
-                | _ -> printfn "Could not find day"
+
+    let modules =
+        Assembly.GetExecutingAssembly().GetTypes()
+        |> Array.filter (fun t -> t.Name.StartsWith("Day"))
+
+    try
+        let read = Console.ReadLine() |> int
+
+        let chosenModule =
+            modules
+            |> Seq.find (fun m -> m.Name = $"Day%02i{read}")
+
+        let solveMethod = chosenModule.GetMethod("solve")
+
+        solveMethod.Invoke(null, null) |> ignore
+    with
+    | _ -> printfn "Could not find day"
 
     prompt forever
-   
-[<EntryPoint>] 
-prompt()
+
+[<EntryPoint>]
+prompt ()
