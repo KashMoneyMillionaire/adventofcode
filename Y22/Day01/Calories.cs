53dea709dc41609fc74130626c50e565cad8c137
kash.cummings <kash.cummings@care.com>
2022-12-01 14:22:35 -0500

    add 22.1

---
+++ Y22/Day01/Calories.cs
@@ -0,0 +1,25 @@
+﻿namespace Y22.Day01;
+
+[DayOf(1)]
+public class Calories : IDaySolver
+{
+    public static object SolvePart1(string input)
+    {
+        return input.Split($"{Environment.NewLine}{Environment.NewLine}")
+                    .Select(l => l.Split(Environment.NewLine)
+                                  .Select(int.Parse)
+                                  .Sum())
+                    .Max();
+    }
+
+    public static object SolvePart2(string input)
+    {
+        return input.Split($"{Environment.NewLine}{Environment.NewLine}")
+                    .Select(l => l.Split(Environment.NewLine)
+                                  .Select(int.Parse)
+                                  .Sum())
+                    .OrderDescending()
+                    .Take(3)
+                    .Sum();
+    }
+}
---
+++ Y22/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+Console.Write("Input: ");
+string result = Console.ReadLine() switch
+{
+    [..var day, '.', var part] => Solve(int.Parse(day), part, GetInput(int.Parse(day), "input.txt")),
+    [..var day, '.', var part, '.'] => Solve(int.Parse(day), part, GetInput(int.Parse(day), "test.txt")),
+    _ => "Unknown"
+};
+
+result.Print("The solution is: ");
+Console.Read();
+
+
+string Solve(int day, char part, string input)
+{
+    var solver = typeof(IDaySolver).Assembly
+                                   .GetTypes()
+                                   .Where(t => t.IsAssignableTo(typeof(IDaySolver))
+                                               && !t.IsInterface)
+                                   .Single(t => t.GetCustomAttribute<DayOfAttribute>()?.Day == day);
+
+    try
+    {
+        return solver.GetMethod($"SolvePart{part}").Invoke(null, new object[] { input }).ToString();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return "Error";
+    }
+}
+
+string GetInput(int day, string fileName)
+{
+    return File.ReadAllText($"Day{day:00}/{fileName}");
+}
+
+public interface IDaySolver
+{
+    static abstract object SolvePart1(string input);
+    static abstract object SolvePart2(string input);
+}
+
+public class DayOfAttribute : Attribute
+{
+    public int Day { get; set; }
+
+    public DayOfAttribute(int dayNumber)
+    {
+        Day = dayNumber;
+    }
+}
+
+public static class Extensions
+{
+    public static void Print(this string value, string prefix) => Console.WriteLine($"{prefix}{value}");
+}
