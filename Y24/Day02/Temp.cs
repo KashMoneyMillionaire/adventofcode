23f03dc5750847e3984ccfb88a099f057109bc76
Kash Cummings <KashMoneyMillionaire@users.noreply.github.com>
2024-12-03 17:16:01 +0000

    day 2 init

---
+++ Y24/Day02/Temp.cs
@@ -0,0 +1,15 @@
+namespace Y24.Day02;
+
+[DayOf(02)]
+public class Day02 : IDaySolver
+{
+    public static object SolvePart1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static object SolvePart2(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
