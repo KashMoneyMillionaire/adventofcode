19180d019de11e3363b363ca17e12d5da76cf9a7
Kash Cummings <KashMoneyMillionaire@users.noreply.github.com>
2024-12-12 15:15:22 +0000

    gen day 5

---
+++ Y24/Day05/Temp.cs
@@ -0,0 +1,15 @@
+namespace Y24.Day05;
+
+[DayOf(05)]
+public class Day05 : IDaySolver
+{
+    public static object SolvePart1(string input)
+    {
+        return input;
+    }
+
+    public static object SolvePart2(string input)
+    {
+        return input;
+    }
+}
